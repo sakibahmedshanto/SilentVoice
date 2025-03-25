b9c2c4f11e23a599ad1749d107f6263f783559d8
sakibahmedshanto <sakibsanto57@gmail.com>
2025-03-25 14:33:27 +0600

    created a layout for the admin page and used the navigation bar there

--- silent-voice/src/components/MobileSidebar.tsx
+++ silent-voice/src/components/MobileSidebar.tsx
@@ -1,46 +1,59 @@
+"use client"
 import { userUserLoaded, useUser } from "@/hooks/user";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import Spinner from "./Spinner";
 
 type MobileSidebarProps = {
     show: boolean;
-    setShow:() => void;
-}
+    setShow: () => void;
+};
 
 const navsmenu = [
-    {label: 'Sign Detection', href: '/sign-detection'},
-    {label: 'Dictionary', href: '/dictionary'},
-    {label: 'Quiz', href: '/quiz'},
-]
-const MobileSidebar = ({ show, setShow }:MobileSidebarProps) => {
-    const router = useRouter();
-    const [user, _] = useUser();
-    const [loaded, __] = userUserLoaded();
+    { label: 'Sign Detection', href: '/sign-detection' },
+    { label: 'Dictionary', href: '/dictionary' },
+    { label: 'Quiz', href: '/quiz' },
+];
+
+const MobileSidebar = ({ show, setShow }: MobileSidebarProps) => {
+    const pathname = usePathname();
+    const [user] = useUser();
+    const [loaded] = userUserLoaded();
 
     return (
         <div className={`fixed top-0 left-0 h-screen right-0 bottom-0 ${show ? "z-[1]" : "-z-10 invisible"}`}>
-            <div className="absolute bg-black bg-opacity-50 top-0 left-0 right-0 bottom-0 z-0" onClick={setShow}/>
+            <div className="absolute bg-black bg-opacity-50 top-0 left-0 right-0 bottom-0 z-0" onClick={setShow} />
             <div className="bg-white relative z-10 h-screen overflow-auto max-w-60">
                 <div className="grid gap-4">
-                    {
-                        navsmenu.map(n => <Link href={n.href}
-                            className={`px-6 py-2 block hover:bg-primary hover:text-white ${router.pathname == n.href ? "bg-blue-700 text-white" : ""}`}
-                            key={n.href}>{n.label}</Link>)
-                    }
-                    {
-                        !loaded ? <Spinner size={1}/>
-                        :
+                    {navsmenu.map((n) => (
+                        <Link
+                            href={n.href}
+                            className={`px-6 py-2 block hover:bg-primary hover:text-white ${
+                                pathname === n.href ? "bg-blue-700 text-white" : ""
+                            }`}
+                            key={n.href}
+                        >
+                            {n.label}
+                        </Link>
+                    ))}
+                    {!loaded ? (
+                        <Spinner size={1} />
+                    ) : (
                         !user && (
-                            <Link href={'/login'}
-                                className={`px-6 py-2 block hover:bg-primary hover:text-white ${router.pathname == '/login' ? "bg-blue-700 text-white" : ""}`}
-                            >Login</Link>
+                            <Link
+                                href={'/login'}
+                                className={`px-6 py-2 block hover:bg-primary hover:text-white ${
+                                    pathname === '/login' ? "bg-blue-700 text-white" : ""
+                                }`}
+                            >
+                                Login
+                            </Link>
                         )
-                    }
+                    )}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default MobileSidebar;
